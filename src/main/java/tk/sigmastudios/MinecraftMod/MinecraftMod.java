f42c78988a6f83d59554525b74efc33abb4e0142
MonstahGames <monstahhhgames@gmail.com>
2017-09-24 00:43:47 +0200

    -

--- src/main/java/tk/sigmastudios/MinecraftMod/MinecraftMod.java
+++ src/main/java/tk/sigmastudios/MinecraftMod/MinecraftMod.java
@@ -15,7 +15,7 @@ import tk.sigmastudios.MinecraftMod.tab.CreativeTab;
 public class MinecraftMod
 {
     public static final String MODID = "monstahmod";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.0";
     public static final String NAME = "Monstahhh Mod";
     
     @SidedProxy(clientSide = "tk.sigmastudios.MinecraftMod.proxy.ClientProxy", serverSide = "tk.sigmastudios.MinecraftMod.proxy.CommonProxy")
