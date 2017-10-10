bf776f63e8e8a742a193733db050f545b4c59ba7
MonstahGames <monstahhhgames@gmail.com>
2017-10-10 19:08:59 +0200

    Added Client and Server proxy

---
+++ MonstahMod/src/main/java/com/sigmastudios/monstahmod/proxy/CommonProxy.java
@@ -0,0 +1,22 @@
+package com.sigmastudios.monstahmod.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy
+{
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+    //13:57
+}
