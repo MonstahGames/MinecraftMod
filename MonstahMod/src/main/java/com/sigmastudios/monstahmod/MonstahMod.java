bb480a60b0d85aae4a96f534a84269214ac9494b
MonstahGames <monstahhhgames@gmail.com>
2017-10-10 18:18:02 +0200

    Added base mod on 1.12.2

---
+++ MonstahMod/src/main/java/com/sigmastudios/monstahmod/MonstahMod.java
@@ -0,0 +1,18 @@
+package com.sigmastudios.monstahmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MonstahMod.MODID, version = MonstahMod.VERSION)
+public class MonstahMod
+{
+    public static final String MODID = "monstahmod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
